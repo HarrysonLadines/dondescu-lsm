ae6739a0ecab5021aaf4303252d152b609dcb265
Jonathan Altamirano <jona-mo-7@hotmail.com>
2026-03-31 12:07:45 -0300

    feat: creacion de modulos producto

---
+++ backend/src/product/product.module.ts
@@ -0,0 +1,13 @@
+import { Module } from "@nestjs/common";
+import { ProductController } from "./product.controller";
+import { ProductService } from "./product.service";
+import { PrismaModule } from "src/prisma/prisma.module";
+
+@Module({
+    controllers: [ProductController],
+    providers: [ProductService],
+    imports: [PrismaModule]
+})
+export class ProductModule {
+
+}
