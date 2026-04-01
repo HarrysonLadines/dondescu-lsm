ac1e3580f92f68adf8a04ee34aa2558c004490cb
HarrysonLadines <harrysonladinesarg@gmail.com>
2026-04-01 17:10:55 -0300

    feat: crear CRUD de productos

---
+++ backend/src/producto/producto.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductoDto } from './dto/create-producto.dto';
+import { UpdateProductoDto } from './dto/update-producto.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class ProductoService {
+  constructor(private prisma: PrismaService) { }
+
+  async create(createProductoDto: CreateProductoDto) {
+    return await this.prisma.producto.create({
+      data: createProductoDto,
+    });
+  }
+
+  async findAll() {
+    return await this.prisma.producto.findMany();
+  }
+
+  async findOne(id: string) {
+    return await this.prisma.producto.findUnique({
+      where: { id },
+    });
+  }
+
+  async update(id: string, updateProductoDto: UpdateProductoDto) {
+    return await this.prisma.producto.update({
+      where: { id },
+      data: updateProductoDto,
+    });
+  }
+
+  async remove(id: string) {
+    return await this.prisma.producto.delete({
+      where: { id },
+    });
+  }
+}
