956b655830c9994354c6353e918d3b560ee9f9e7
HarrysonLadines <harrysonladinesarg@gmail.com>
2026-04-08 17:41:21 -0300

    feat: implementar BetterAuth y proteger rutas de productos

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { ProductoModule } from './producto/producto.module.js';
 import { PrismaModule } from './prisma/prisma.module.js';
+import { AuthModule } from './auth/auth.module.js';
 
 @Module({
-  imports: [ProductoModule, PrismaModule],
+  imports: [ProductoModule, PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
